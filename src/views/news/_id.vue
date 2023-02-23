865c8a79595422c0ad29e2788bb7b177ad4e4021
Jamshid Sharipov <sharipoff009@mail.ru>
2023-02-23 20:33:20 +0500

    news and settings added

---
+++ src/views/news/_id.vue
@@ -0,0 +1,89 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="one-news">
+    <img src="@/assets/images/news-one-image.png" alt="">
+    <div class="layout-container">
+      <span class="one-news__date">26.12.2022 15:58</span>
+
+      <p class="one-news__title">Подключи интернет для бизнеса выгодно!</p>
+
+      <div class="one-news__description"></div>
+
+      <div class="one-news__btns flex align-center">
+        <button class="flex align-center">
+          <img src="@/assets/images/like.svg" alt="">
+          <span>100</span>
+        </button>
+        <button class="flex align-center">
+          <img src="@/assets/images/dislike.svg" alt="">
+          <span>3</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.one-news {
+  & img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+
+  &__date {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 133%;
+    color: #A3ABB8;
+  }
+
+  &__title {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 133%;
+    letter-spacing: -0.5px;
+    color: #333333;
+    margin: 1rem 0;
+  }
+
+  &__description {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 138%;
+    letter-spacing: -0.5px;
+    color: #333333;
+  }
+
+  &__btns {
+    margin-top: 24px;
+    & button {
+      padding: 10px;
+      border-radius: 8px;
+      border: none;
+      margin-right: 1rem;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      & img {
+        width: 1rem;
+        height: 1rem;
+        object-fit: contain;
+        margin-right: 4px;
+      }
+
+      & span {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 114%;
+        color: #333333;
+      }
+    }
+  }
+}
+</style>
--- src/views/settings/AppSettings.vue
+++ src/views/settings/AppSettings.vue
@@ -1,3 +1,41 @@
+<script setup>
+
+</script>
+
 <template>
-  <div>Settings</div>
+  <div class="settings">
+    <div class="layout-container">
+      <div class="settings-cards">
+
+        <div class="settings-card">
+          <img src="@/assets/images/lang-icon.svg" alt="">
+          <p>Сменить язык</p>
+        </div>
+
+        <div class="settings-card">
+          <img src="@/assets/images/sound-on-icon.svg" alt="">
+          <p>Сменить язык</p>
+        </div>
+
+        <div class="settings-card">
+          <img src="@/assets/images/document-icon.svg" alt="">
+          <p>Сменить язык</p>
+        </div>
+
+        <div class="settings-card">
+          <img src="@/assets/images/message-icon.svg" alt="">
+          <p>Сменить язык</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+
+.settings {
+
+}
+
+</style>
