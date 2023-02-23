8782aeb3a04f2b94a187b85a5278c7c8567974e6
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-02-23 16:33:21 +0500

    bonus started

--- src/views/bonus/AppBonus.vue
+++ src/views/bonus/AppBonus.vue
@@ -1,3 +1,71 @@
+<script setup>
+
+</script>
+
+
 <template>
-  <div>app bonus</div>
+    <div>
+
+        <div class="bonus-block">
+
+        </div>
+
+        <div class="gifts-block">
+            <div class="gift-card">
+                <div class="gift-card__image">
+                    <img src="@/assets/images/home-card-layout.png" alt="">
+                </div>
+                <div class="gift-card__content">
+                    <h5>Наушники </h5>
+                    <div class="price">
+                        <img src="@/assets/images/coin.png" alt="">
+                        <p>1000</p>
+                    </div>
+                </div>
+                <div class="gift-card__button">
+                    <p>В корзину</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+
+<style lang="scss" scoped>
+.gift-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    row-gap: 1rem;
+    padding: 16px 10px;
+    max-width: 161px;
+
+    &__image {
+        width: 100px;
+        height: 100px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+
+    &__content {
+        display: flex;
+        flex-direction: column;
+        h5 {
+            @extend .head-h5;
+        }
+        .price{
+            display: flex;
+            column-gap: .5rem;
+            img {
+                max-width: 100%;
+                object-fit: contain;
+            }
+        }
+    }
+}
+</style>
