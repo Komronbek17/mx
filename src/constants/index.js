ebbd37c27ddad20f56ba83e16812fc81f6715c49
komronsultonov <sultonovkomronbek17@gmail.com>
2023-02-27 15:26:26 +0500

    Add Back And Main Buttons Controller

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const OLTIN_BALIQ_BOT_TKN = "cd7e678d-7d38-4e1e-b94f-c432e690abb3";
 export const TELEGRAM = "Telegram";
 export const WEB_APP = "WebApp";
-export const BACK_BUTTON = "backButton";
+export const BACK_BUTTON = "BackButton";
 export const MAIN_BUTTON = "MainButton";
--- src/stores/telegram.store.js
+++ src/stores/telegram.store.js
@@ -1,7 +1,5 @@
-import { computed, reactive, ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
-import { MainButtonController } from "@/utils/telegram/main.button.controller";
-import { BackButtonController } from "@/utils/telegram/back.button.controller";
 
 export const useTelegramStore = defineStore("telegram", () => {
   const webApp = ref({
@@ -15,10 +13,6 @@ export const useTelegramStore = defineStore("telegram", () => {
       },
     },
   });
-  const backButtonController = new BackButtonController({});
-  const mainButtonController = reactive({
-    controller: new MainButtonController({}),
-  });
 
   const tUserId = computed(() => webApp.value.initDataUnsafe.user.id);
   const tUser = computed(() => webApp.value.initDataUnsafe.user);
@@ -29,30 +23,13 @@ export const useTelegramStore = defineStore("telegram", () => {
   function initWebApp({ tApp }) {
     webApp.value = tApp;
   }
-  function initBackButton({ route, router }) {
-    backButtonController.value = new BackButtonController({
-      route,
-      router,
-    });
-  }
 
-  function initMainButton({ route, router }) {
-    mainButtonController.value = new MainButtonController({
-      route,
-      router,
-    });
-  }
-
-  function initApp({ webApp: tApp, route, router }) {
+  function initApp({ webApp: tApp }) {
     initWebApp({ tApp });
-    initBackButton({ route, router });
-    initMainButton({ route, router });
   }
 
   return {
     webApp,
-    backButtonController,
-    mainButtonController,
     tUserId,
     tUser,
     tUserFullName,
