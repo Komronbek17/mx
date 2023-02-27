904a8b0a39747713fdabd85a89f7c2076662d70b
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-02-27 17:47:24 +0500

    popover updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,7 +17,7 @@ import AppProfileEdit from "@/views/profile/AppProfileEdit.vue";
 import _id from "@/views/news/_id.vue";
 import { getToken } from "@/utils/auth.util";
 import { isNUNEZ } from "@/utils/inspect.util";
-import { useTelegramStore } from "@/stores/telegram.store";
+// import { useTelegramStore } from "@/stores/telegram.store";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
