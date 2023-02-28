4977f46c7b9609ec694a445bc707d382221ff6b4
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-02-28 19:43:56 +0500

    app market full fixed

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,31 +1,41 @@
 import Core from "@/services/axios/core.axios";
 
-const axiosV2 = import.meta.env.VITE_APP_URL_TEST;
 
-console.log(axiosV2, "axiosV2");
+const axiosV2 = import.meta.env.VITE_APP_URL_TEST
+
+console.log(axiosV2,'axiosV2');
 
 class ProductService extends Core {
-  constructor() {
-    super({
-      endpoint: "coin/",
-    });
-  }
 
-  fetchProducts(body) {
-    return this.post("products", body);
-  }
+    constructor() {
+        super({
+            endpoint: "coin/",
+        });
+    }
 
-  setDailyGift() {
-    return this.post("");
-  }
+    fetchProducts(body) {
+        return this.post("products", body);
+    }
 
-  fetchPremiumLampInfo() {
-    return this.get("premium");
-  }
+    getBalance() {
+        return this.post("balance");
+    }
 
-  setPremiumLampGift() {
-    return this.post("premium");
-  }
+    activateProduct(body){
+        return this.post("products/activation", body);
+    }
+
+    setDailyGift() {
+        return this.post("");
+    }
+
+    fetchPremiumLampInfo() {
+        return this.get("premium");
+    }
+
+    setPremiumLampGift() {
+        return this.post("premium");
+    }
 }
 
 export const productApi = new ProductService();
--- src/views/market/AppMarket.vue
+++ src/views/market/AppMarket.vue
@@ -1,185 +1,161 @@
 <script setup>
+
+import {useRouter} from "vue-router";
 import {useToast} from "vue-toastification";
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 import {productApi} from "@/services/product.service";
-
 import ModalDialog from "@/components/ui/ModalDialog/ModalDialog.vue";
-import AppLoader from "@/components/elements/loader/AppLoader.vue";
-import {loadingComposable} from "@/composables/loading.composable";
-import {useI18n} from "vue-i18n";
 
-const {t} = useI18n();
-
-const {
-  loading: isFetching,
-  startLoading,
-  finishLoading,
-} = loadingComposable();
+const router = useRouter();
 const toast = useToast();
-const gifts = ref([]);
-const balance = ref(0);
 
-const modalValue = ref(false);
-const activeId = ref(null);
+const gifts = ref([])
+const balance = ref(0)
+
+const modalValue = ref(false)
+const activeId = ref(null)
 
 const getProducts = async () => {
-  try {
-    const body = {
-      method: "coin.get_all_products",
-      params: {
-        page: 1,
-        limit: 10,
-      },
-    };
-    await productApi.fetchProducts(body).then((response) => {
-      gifts.value = response.data.result;
-    });
-  } catch (e) {
-    toast.error(e?.response?.data?.message);
-  }
-};
+    try {
+        const body = {
+            method: "coin.get_all_products",
+            params: {
+                page: 1,
+                limit: 10,
+            }
+        }
+        const {data} = await productApi.fetchProducts(body);
+        gifts.value = data.result
+
+    } catch ({response}) {
+        toast.error(response?.data?.message);
+    }
+}
+
 
 const fetchBalance = async () => {
-  try {
-    await productApi.getBalance().then((response) => {
-      balance.value = response.data.balance;
-    });
-  } catch (e) {
-    toast.error(e?.response?.data?.message);
-  }
-};
+    try {
+        const {data} = await productApi.getBalance();
+
+        balance.value = data.balance
+
+    } catch ({response}) {
+        toast.error(response?.data?.message);
+    }
+}
 const askActivate = (id) => {
-  activeId.value = id;
-  openModal();
-};
+    activeId.value = id
+    openModal()
+}
+
 
 const closeDialogModal = () => {
-  modalValue.value = false;
-  activeId.value = null;
-};
+    modalValue.value = false
+    activeId.value = null
+}
 
 const openModal = () => {
-  modalValue.value = true;
-};
+    modalValue.value = true
+}
 const modalApply = () => {
-  submitActive();
-  modalValue.value = false;
-};
+    submitActive()
+    modalValue.value = false
+}
 
-const addBasket = (item) => {
-  console.log(item, "addBasket");
-};
+
+const addBasket = () => {
+    console.log('addBasket');
+}
 
 const submitActive = async () => {
-  if (activeId.value) {
-    const body = {
-      method: "coin.activation_product",
-      params: {
-        id: activeId.value,
-      },
-    };
+    if (activeId.value) {
+        const body = {
+            method: 'coin.activation_product',
+            params: {
+                id: activeId.value
+            }
+        }
 
-    try {
-      const {data} = await productApi.activateProduct(body);
-      // console.log(data,'data');
-      gifts.value = data.result;
-    } catch (e) {
-      console.log(e, "getProducts();");
-      toast.error(e.response?.data?.message ?? e.message);
+        try {
+            const {data} = await productApi.activateProduct(body)
+            console.log(data);
+        } catch (e) {
+            toast.error(e.response?.data?.message ?? e.message);
+        }
+
     }
-  }
-};
 
-onMounted(async () => {
-  startLoading();
-  try {
-    await fetchBalance();
-    await getProducts();
-  } finally {
-    finishLoading();
-  }
-});
+}
+
+
+fetchBalance()
+getProducts()
+
 </script>
 
+
 <template>
-  <div class="layout-container">
-    <app-loader :active="isFetching"/>
-    <div class="bonus-block">
-      <div class="bonus-card">
-        <div class="bonus-card__title">{{ $t("market_page.balance") }}:</div>
-        <div class="bonus-card__price">
-          <img src="@/assets/images/coin.png" alt=""/>
-          <p>{{ balance }}</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="gifts-block">
-      <div class="gift-title">{{ t("market_page.prize") }}</div>
-      <div class="gift-list">
-        <router-link
-            v-for="gift in gifts"
-            :key="gift.id + '_level_1'"
-            :to="{name:'market-product', params:{id:gift.id}}"
-            class="gift-card"
-        >
-          <div class="gift-card__image">
-            <img
-                :src="gift.images[0]?.path || '@/assets/images/no-photo.svg'"
-                alt=""
-            />
-          </div>
-          <div class="gift-card__content">
-            <h5>{{ gift.name }}</h5>
-            <div class="price">
-              <img src="@/assets/images/coin.png" alt=""/>
-              <p>{{ gift.price }}</p>
+    <div class="layout-container">
+        <div class="bonus-block">
+            <div class="bonus-card">
+                <div class="bonus-card__title">Баланс:</div>
+                <div class="bonus-card__price">
+                    <img src="@/assets/images/coin.png" alt=""/>
+                    <p>{{ balance }}</p>
+                </div>
             </div>
-          </div>
-          <div
-              v-if="gift.type === 'product'"
-              @click="addBasket(gift.id)"
-              class="gift-card__button"
-          >
-            <p>{{ t("market_page.to_basket") }}</p>
-          </div>
-          <div v-else @click="askActivate(gift.id)" class="gift-card__button">
-            <p>{{ t("market_page.activate") }}</p>
-          </div>
-        </router-link>
-      </div>
-    </div>
+        </div>
 
-    <modal-dialog :model-value="modalValue" @close-modal="closeDialogModal">
-      <template #header>
-        <div class="modal-header">
-          <img src="@/assets/images/2x-blue.png" alt=""/>
+        <div class="gifts-block">
+            <div class="gift-title">
+                Призы
+            </div>
+            <div class="gift-list">
+                <div v-for="gift in gifts" :key="gift.id + '_level_1'" class="gift-card">
+                    <div class="gift-card__image">
+                        <img :src="gift.images[0]?.path || '@/assets/images/no-photo.svg'" alt="">
+                    </div>
+                    <div class="gift-card__content">
+                        <h5>{{ gift.name }}</h5>
+                        <div class="price">
+                            <img src="@/assets/images/coin.png" alt="">
+                            <p>{{ gift.price }}</p>
+                        </div>
+                    </div>
+                    <div v-if="gift.type ==='product'" @click="addBasket(gift.id)" class="gift-card__button">
+                        <p>В корзину</p>
+                    </div>
+                    <div v-else @click="askActivate(gift.id)" class="gift-card__button">
+                        <p>Активировать</p>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-      </template>
-      <template #content>
-        <div class="modal-content">
-          <h3 class="modal-content__title">
-            {{ t("market_page.activated") }}!
-          </h3>
-          <p class="modal-content__subtitle">
-            {{
-              t("connect_premium_service_message", {
-                level: 1,
-              })
-            }}!
-          </p>
-        </div>
-      </template>
-      <template #footer>
-        <div class="modal-footer">
-          <div @click="modalApply" class="modal-footer__button btn-yellow">
-            {{ t("ok") }}
-          </div>
-        </div>
-      </template>
-    </modal-dialog>
-  </div>
+
+        <modal-dialog :model-value="modalValue" @close-modal="closeDialogModal">
+            <template #header>
+                <div class="modal-header">
+                    <img src="@/assets/images/2x-blue.png" alt="">
+                </div>
+            </template>
+            <template #content>
+                <div class="modal-content">
+                    <h3 class="modal-content__title">Активировано!</h3>
+                    <p class="modal-content__subtitle">2Х бонус 1го уровня успешно активирован!</p>
+                </div>
+            </template>
+            <template #footer>
+                <div class="modal-footer">
+                    <div @click="modalApply" class="modal-footer__button btn-yellow">Окей</div>
+                </div>
+            </template>
+        </modal-dialog>
+
+    </div>
 </template>
+
 
 <style lang="scss" scoped>
 @import "market-style";
