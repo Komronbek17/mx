0412c693422cc4d3f4c72dce3f6a87cefd3d7726
komronsultonov <sultonovkomronbek17@gmail.com>
2023-03-01 14:37:35 +0500

    change base url of the history service

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,41 +1,35 @@
 import Core from "@/services/axios/core.axios";
 
-
-const axiosV2 = import.meta.env.VITE_APP_URL_TEST
-
-console.log(axiosV2,'axiosV2');
-
 class ProductService extends Core {
+  constructor() {
+    super({
+      endpoint: "coin/",
+    });
+  }
 
-    constructor() {
-        super({
-            endpoint: "coin/",
-        });
-    }
+  fetchProducts(body) {
+    return this.post("products", body);
+  }
 
-    fetchProducts(body) {
-        return this.post("products", body);
-    }
+  getBalance() {
+    return this.post("balance");
+  }
 
-    getBalance() {
-        return this.post("balance");
-    }
+  activateProduct(body) {
+    return this.post("products/activation", body);
+  }
 
-    activateProduct(body){
-        return this.post("products/activation", body);
-    }
+  setDailyGift() {
+    return this.post("");
+  }
 
-    setDailyGift() {
-        return this.post("");
-    }
+  fetchPremiumLampInfo() {
+    return this.get("premium");
+  }
 
-    fetchPremiumLampInfo() {
-        return this.get("premium");
-    }
-
-    setPremiumLampGift() {
-        return this.post("premium");
-    }
+  setPremiumLampGift() {
+    return this.post("premium");
+  }
 }
 
 export const productApi = new ProductService();
