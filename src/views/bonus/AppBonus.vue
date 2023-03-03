9deb1ec57ef2db15ff126e84c09857e997cdb690
komronsultonov <sultonovkomronbek17@gmail.com>
2023-03-03 14:54:24 +0500

    Complete profile page

--- src/views/bonus/AppBonus.vue
+++ src/views/bonus/AppBonus.vue
@@ -1,108 +1,110 @@
 <script setup>
-import {useRoute} from "vue-router";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 </script>
 
 <template>
-    <div class="history">
-        <div class="history-block">
-            <div class="history-tabs">
-                <!--        <router-link-->
-                <!--          :to="{ name: 'bonus-active', params: {} }"-->
-                <!--          class="history-tab"-->
-                <!--          :class="route.name === 'bonus-active' ? 'active' : ''"-->
-                <!--        >-->
-                <!--          Активные-->
-                <!--        </router-link>-->
-                <router-link
-                    :to="{ name: 'bonus-recent', params: {} }"
-                    class="history-tab"
-                    :class="route.name === 'bonus-recent' ? 'active' : ''"
-                >
-                    Недавние
-                </router-link>
-                <router-link
-                    :to="{ name: 'bonus-prize', params: {} }"
-                    class="history-tab"
-                    :class="route.name === 'bonus-prize' ? 'active' : ''"
-                >
-                    Призы
-                </router-link>
-                <!--        <router-link-->
-                <!--          :to="{ name: 'bonus-archive', params: {} }"-->
-                <!--          class="history-tab"-->
-                <!--          :class="route.name === 'bonus-archive' ? 'active' : ''"-->
-                <!--        >-->
-                <!--          Архив-->
-                <!--        </router-link>-->
-            </div>
-        </div>
-        <router-view/>
+  <div class="history">
+    <div class="history-block">
+      <div class="history-tabs">
+        <!--        <router-link-->
+        <!--          :to="{ name: 'bonus-active', params: {} }"-->
+        <!--          class="history-tab"-->
+        <!--          :class="route.name === 'bonus-active' ? 'active' : ''"-->
+        <!--        >-->
+        <!--          Активные-->
+        <!--        </router-link>-->
+        <router-link
+          :to="{ name: 'bonus-recent', params: {} }"
+          class="history-tab"
+          :class="route.name === 'bonus-recent' ? 'active' : ''"
+        >
+          Недавние
+        </router-link>
+        <router-link
+          :to="{ name: 'bonus-prize', params: {} }"
+          class="history-tab"
+          :class="route.name === 'bonus-prize' ? 'active' : ''"
+        >
+          Призы
+        </router-link>
+        <!--        <router-link-->
+        <!--          :to="{ name: 'bonus-archive', params: {} }"-->
+        <!--          class="history-tab"-->
+        <!--          :class="route.name === 'bonus-archive' ? 'active' : ''"-->
+        <!--        >-->
+        <!--          Архив-->
+        <!--        </router-link>-->
+      </div>
     </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 ::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 .history {
-    padding: 0;
+  padding: 0;
 
-    &-block {
-        position: relative;
-        z-index: 0;
+  &-block {
+    position: relative;
+    z-index: 0;
 
-        &::after {
-            content: "";
-            width: 100%;
-            display: block;
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            height: 2px;
-            background-color: #f5f5f5;
-        }
+    &::after {
+      content: "";
+      width: 100%;
+      display: block;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      height: 2px;
+      background-color: #f5f5f5;
     }
+  }
 
-    &-tabs {
-        position: relative;
-        padding: 0 1rem;
-        overflow-x: auto;
-        white-space: nowrap;
-        display: flex;
-        column-gap: 20px;
+  &-tabs {
+    position: relative;
+    padding: 0 1rem;
+    overflow-x: auto;
+    white-space: nowrap;
+    display: flex;
+    column-gap: 20px;
+  }
+
+  &-tab {
+    position: relative;
+    font-weight: 600;
+    font-size: 17px;
+    line-height: 129%;
+    color: #dbdbdb;
+    text-decoration: none;
+    z-index: 1232;
+    padding: 16px 0 12px;
+
+    &.active {
+      @include text-gradient(
+        linear-gradient(107.32deg, #4adaff -22.08%, #0062ca 122.03%)
+      );
+
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        display: block;
+        width: 100%;
+        background: linear-gradient(
+          107.32deg,
+          #4adaff -22.08%,
+          #0062ca 122.03%
+        );
+        height: 2px;
+        border-radius: 100px;
+      }
     }
-
-    &-tab {
-        position: relative;
-        font-weight: 600;
-        font-size: 17px;
-        line-height: 129%;
-        color: #dbdbdb;
-        text-decoration: none;
-        z-index: 1232;
-        padding: 16px 0 12px;
-
-        &.active {
-            @include text-gradient(linear-gradient(107.32deg, #4adaff -22.08%, #0062ca 122.03%));
-
-            &::after {
-                content: "";
-                position: absolute;
-                bottom: 0;
-                display: block;
-                width: 100%;
-                background: linear-gradient(
-                        107.32deg,
-                        #4adaff -22.08%,
-                        #0062ca 122.03%
-                );
-                height: 2px;
-                border-radius: 100px;
-            }
-        }
-    }
+  }
 }
 </style>
--- src/views/news/_id.vue
+++ src/views/news/_id.vue
@@ -1,148 +1,150 @@
 <script setup>
+import { newsApi } from "@/services/news.service";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useToast } from "vue-toastification";
 
-import {newsApi} from "@/services/news.service";
-import {onMounted, ref} from "vue";
-import {useRoute} from "vue-router";
-import {useToast} from "vue-toastification";
-
-const route = useRoute()
-const newData = ref({})
+const route = useRoute();
+const newData = ref({});
 
 const toast = useToast();
 
-
 const body = {
-    method: "news.get_one",
-    params: {
-        id: route.params.id,
-    }
-}
+  method: "news.get_one",
+  params: {
+    id: route.params.id,
+  },
+};
 
 const getNewData = async () => {
-    try {
-        const {data} = await newsApi.fetchOneNews(body)
-        newData.value = data.result
-        console.log(data, 'newsApi');
-    } catch ({response}) {
-        toast.error(response?.data?.message);
-    }
-}
-
+  try {
+    const { data } = await newsApi.fetchOneNews(body);
+    newData.value = data.result;
+    console.log(data, "newsApi");
+  } catch ({ response }) {
+    toast.error(response?.data?.message);
+  }
+};
 
 const onLike = async () => {
-    try {
-        const {data} = await newsApi.setLike(body)
-        newData.value = data.result
-    } catch ({response}) {
-        toast.error(response?.data?.message);
-    }
-}
-
+  try {
+    const { data } = await newsApi.setLike(body);
+    newData.value = data.result;
+  } catch ({ response }) {
+    toast.error(response?.data?.message);
+  }
+};
 
 const onDislike = async () => {
-    try {
-        const {data} = await newsApi.setDislike(body)
-        newData.value = data.result
-    } catch ({response}) {
-        toast.error(response?.data?.message);
-    }
-}
-
+  try {
+    const { data } = await newsApi.setDislike(body);
+    newData.value = data.result;
+  } catch ({ response }) {
+    toast.error(response?.data?.message);
+  }
+};
 
 onMounted(() => {
-    getNewData()
-})
+  getNewData();
+});
 </script>
 
 <template>
-    <div class="one-news">
-        <img :src="newData.image" alt=""/>
-        <div class="layout-container">
-            <span class="one-news__date">{{ newData.date }}</span>
+  <div class="one-news">
+    <img :src="newData.image" alt="" />
+    <div class="layout-container">
+      <span class="one-news__date">{{ newData.date }}</span>
 
-            <p class="one-news__title">{{ newData.name }}</p>
+      <p class="one-news__title">{{ newData.name }}</p>
 
-            <div class="one-news__description">
-                {{ newData.description }}
-            </div>
+      <div class="one-news__description">
+        {{ newData.description }}
+      </div>
 
-            <div v-if="newData.likes" class="one-news__btns flex align-center">
-                <button @click="onLike" class="flex align-center">
-                    <img v-if="newData.is_like" src="@/assets/images/like-fill.svg" alt=""/>
-                    <img v-else src="@/assets/images/like.svg" alt=""/>
-                    <span> {{ newData.likes.like }}</span>
-                </button>
-                <button @click="onDislike" class="flex align-center">
-                    <img v-if="newData.is_dislike" src="@/assets/images/dislike-fill.svg" alt=""/>
-                    <img v-else src="@/assets/images/dislike.svg" alt=""/>
-                    <span>{{ newData.likes.dislike }}</span>
-                </button>
-            </div>
-        </div>
+      <div v-if="newData.likes" class="one-news__btns flex align-center">
+        <button @click="onLike" class="flex align-center">
+          <img
+            v-if="newData.is_like"
+            src="@/assets/images/like-fill.svg"
+            alt=""
+          />
+          <img v-else src="@/assets/images/like.svg" alt="" />
+          <span> {{ newData.likes.like }}</span>
+        </button>
+        <button @click="onDislike" class="flex align-center">
+          <img
+            v-if="newData.is_dislike"
+            src="@/assets/images/dislike-fill.svg"
+            alt=""
+          />
+          <img v-else src="@/assets/images/dislike.svg" alt="" />
+          <span>{{ newData.likes.dislike }}</span>
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .one-news {
-    & img {
-        width: 100%;
-        height: 200px;
-        object-fit: cover;
-    }
+  & img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+  }
 
-    &__date {
-        font-weight: 400;
+  &__date {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 133%;
+    color: #a3abb8;
+  }
+
+  &__title {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 133%;
+    letter-spacing: -0.5px;
+    color: #333333;
+    margin: 1rem 0;
+  }
+
+  &__description {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 138%;
+    letter-spacing: -0.5px;
+    color: #333333;
+  }
+
+  &__btns {
+    margin-top: 24px;
+
+    & button {
+      padding: 10px;
+      border-radius: 8px;
+      border: none;
+      margin-right: 1rem;
+      background: #f5f5f5;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      & img {
+        width: 1rem;
+        height: 1rem;
+        object-fit: contain;
+        margin-right: 4px;
+      }
+
+      & span {
+        font-weight: 500;
         font-size: 14px;
-        line-height: 133%;
-        color: #a3abb8;
-    }
-
-    &__title {
-        font-weight: 600;
-        font-size: 18px;
-        line-height: 133%;
-        letter-spacing: -0.5px;
+        line-height: 114%;
         color: #333333;
-        margin: 1rem 0;
+      }
     }
-
-    &__description {
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 138%;
-        letter-spacing: -0.5px;
-        color: #333333;
-    }
-
-    &__btns {
-
-        margin-top: 24px;
-
-        & button {
-            padding: 10px;
-            border-radius: 8px;
-            border: none;
-            margin-right: 1rem;
-            background: #F5F5F5;
-
-            &:last-child {
-                margin-right: 0;
-            }
-
-            & img {
-                width: 1rem;
-                height: 1rem;
-                object-fit: contain;
-                margin-right: 4px;
-            }
-
-            & span {
-                font-weight: 500;
-                font-size: 14px;
-                line-height: 114%;
-                color: #333333;
-            }
-        }
-    }
+  }
 }
 </style>
