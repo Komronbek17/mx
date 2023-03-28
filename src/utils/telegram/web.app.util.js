4e2e66abc711a89d7215c1845563593a2e300d53
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-03-28 13:39:50 +0500

    call center number fixed

--- src/utils/telegram/web.app.util.js
+++ src/utils/telegram/web.app.util.js
@@ -192,4 +192,11 @@ export class WebAppController {
       });
     }
   }
+
+
+  static checkAndroidDevice(){
+    if(navigator){
+      return !!navigator.userAgent.includes('Android')
+    }
+  }
 }
