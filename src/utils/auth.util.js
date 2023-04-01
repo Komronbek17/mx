12a567de91e80f798c3805d433272a1a9d3a4fbb
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-04-01 18:09:06 +0500

    bearer

--- src/utils/auth.util.js
+++ src/utils/auth.util.js
@@ -1,4 +1,5 @@
 import {
+  getLocalStorageVariable,
   // getLocalStorageVariable,
   removeLocalStorageVariable,
   setLocalStorageVariable,
@@ -10,8 +11,8 @@ export function setToken(token) {
 }
 
 export function getToken() {
-  return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nb2xkZW5maXNoLjFpdC51elwvdjFcL29hdXRoXC92ZXJpZnkiLCJpYXQiOjE2ODAxNTUyNjUsImV4cCI6MTcxMTY5MTI2NSwibmJmIjoxNjgwMTU1MjY1LCJqdGkiOiJIUWdZN2Y1UTNZVmREOXRUIiwic3ViIjoyNiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.r3obIN5lSmirumlP2C_H5WgYQF19vWGyYS3Lkb-Bgg8";
-  // return getLocalStorageVariable(OLTIN_BALIQ_BOT_TKN);
+  // return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nb2xkZW5maXNoLjFpdC51elwvdjFcL29hdXRoXC92ZXJpZnkiLCJpYXQiOjE2ODAxNTUyNjUsImV4cCI6MTcxMTY5MTI2NSwibmJmIjoxNjgwMTU1MjY1LCJqdGkiOiJIUWdZN2Y1UTNZVmREOXRUIiwic3ViIjoyNiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.r3obIN5lSmirumlP2C_H5WgYQF19vWGyYS3Lkb-Bgg8";
+  return getLocalStorageVariable(OLTIN_BALIQ_BOT_TKN);
 }
 
 export function removeToken() {
