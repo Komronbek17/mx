5072e4d3357b2e60f5a47dd342cb11b30cb0652b
komronsultonov <sultonovkomronbek17@gmail.com>
2023-05-15 18:31:21 +0500

    change order history api

--- src/services/orders.service.js
+++ src/services/orders.service.js
@@ -1,9 +1,9 @@
 import Core from "@/services/axios/core.axios";
-import { axiosV1 } from "@/services/axios/axios";
+import { axiosDev } from "@/services/axios/axios";
 
 class OrdersService extends Core {
   constructor() {
-    super({ axios: axiosV1({}) });
+    super({ axios: axiosDev({}) });
   }
 
   fetchActiveOrders(body) {
